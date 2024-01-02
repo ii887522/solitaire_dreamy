e09a408830b8206f79ce729770ea83e826e524dc
ii887522 <ii887522@gmail.com>
2024-01-02 19:53:28 +0800

    Game size adapt to window size changes

--- lib/games/klondike_game.dart
+++ lib/games/klondike_game.dart
@@ -1,19 +1,46 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_svg/flame_svg.dart';
 import 'package:flutter/material.dart';
 
+final _worldSize = Vector2(360, 422);
+
 class KlondikeGame extends FlameGame {
+  final _worldKey = ComponentKey.unique();
+  var _isIniting = true;
+
   @override
   Color backgroundColor() => Colors.transparent;
+
+  @override
+  void onGameResize(Vector2 size) {
+    super.onGameResize(size);
+
+    // Assume that the world components are defined in the boundary
+    final world = findByKey<PositionComponent>(_worldKey);
+
+    world?.scale = Vector2.all(
+      camera.viewport.size.x / camera.viewport.size.y <
+              _worldSize.x / _worldSize.y
+          ? camera.viewport.size.x / _worldSize.x
+          : camera.viewport.size.y / _worldSize.y,
+    );
+
+    world?.position = Vector2(
+      (max(camera.viewport.size.x, _worldSize.x * world.scale.x) -
+              _worldSize.x * world.scale.x) *
+          0.5,
+      0,
+    );
+  }
 
   @override
   FutureOr<void> onLoad() async {
     camera.viewfinder.anchor = Anchor.topLeft;
     final beginCardGap = Vector2(4, 10);
     final cardGap = Vector2(5, 10);
-    final endCardGap = Vector2(4, 10);
     final cardSize = Vector2(46, 70);
     final suitSize = Vector2.all(40);
 
@@ -32,201 +59,235 @@ class KlondikeGame extends FlameGame {
       style: const TextStyle(fontSize: 48, color: Color(0xFF804080)),
     );
 
-    world.addAll([
-      // StockPile
-      RectangleComponent(
-        position: beginCardGap,
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      SvgComponent(
-        svg: await Svg.load('icons/redo.svg'),
-        anchor: Anchor.center,
-        position: beginCardGap + cardSize * 0.5,
-        size: Vector2.all(32),
-        paint: cardPlaceholderIconPaint,
-      ),
+    world.add(
+      PositionComponent(
+        key: _worldKey,
+        children: [
+          // StockPile
+          RectangleComponent(
+            position: beginCardGap,
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          SvgComponent(
+            svg: await Svg.load('icons/redo.svg'),
+            anchor: Anchor.center,
+            position: beginCardGap + cardSize * 0.5,
+            size: Vector2.all(32),
+            paint: cardPlaceholderIconPaint,
+          ),
 
-      // WastePile
-      RectangleComponent(
-        position: beginCardGap + Vector2(cardSize.x + cardGap.x, 0),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
+          // WastePile
+          RectangleComponent(
+            position: beginCardGap + Vector2(cardSize.x + cardGap.x, 0),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
 
-      // FoundationPileSpade
-      RectangleComponent(
-        position: beginCardGap + Vector2(cardSize.x * 3 + cardGap.x * 3, 0),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      SvgComponent(
-        svg: await Svg.load('icons/spade.svg'),
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 3 + cardGap.x * 3, 0) +
-            cardSize * 0.5,
-        size: suitSize,
-        paint: cardPlaceholderIconPaint,
-      ),
+          // FoundationPileSpade
+          RectangleComponent(
+            position: beginCardGap + Vector2(cardSize.x * 3 + cardGap.x * 3, 0),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          SvgComponent(
+            svg: await Svg.load('icons/spade.svg'),
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(cardSize.x * 3 + cardGap.x * 3, 0) +
+                cardSize * 0.5,
+            size: suitSize,
+            paint: cardPlaceholderIconPaint,
+          ),
 
-      // FoundationPileHeart
-      RectangleComponent(
-        position: beginCardGap + Vector2(cardSize.x * 4 + cardGap.x * 4, 0),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      SvgComponent(
-        svg: await Svg.load('icons/heart.svg'),
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 4 + cardGap.x * 4, 0) +
-            cardSize * 0.5,
-        size: suitSize,
-        paint: cardPlaceholderIconPaint,
-      ),
+          // FoundationPileHeart
+          RectangleComponent(
+            position: beginCardGap + Vector2(cardSize.x * 4 + cardGap.x * 4, 0),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          SvgComponent(
+            svg: await Svg.load('icons/heart.svg'),
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(cardSize.x * 4 + cardGap.x * 4, 0) +
+                cardSize * 0.5,
+            size: suitSize,
+            paint: cardPlaceholderIconPaint,
+          ),
 
-      // FoundationPileClub
-      RectangleComponent(
-        position: beginCardGap + Vector2(cardSize.x * 5 + cardGap.x * 5, 0),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      SvgComponent(
-        svg: await Svg.load('icons/club.svg'),
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 5 + cardGap.x * 5, 0) +
-            cardSize * 0.5,
-        size: suitSize,
-        paint: cardPlaceholderIconPaint,
-      ),
+          // FoundationPileClub
+          RectangleComponent(
+            position: beginCardGap + Vector2(cardSize.x * 5 + cardGap.x * 5, 0),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          SvgComponent(
+            svg: await Svg.load('icons/club.svg'),
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(cardSize.x * 5 + cardGap.x * 5, 0) +
+                cardSize * 0.5,
+            size: suitSize,
+            paint: cardPlaceholderIconPaint,
+          ),
 
-      // FoundationPileDiamond
-      RectangleComponent(
-        position: beginCardGap + Vector2(cardSize.x * 6 + cardGap.x * 6, 0),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      SvgComponent(
-        svg: await Svg.load('icons/diamond.svg'),
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 6 + cardGap.x * 6, 0) +
-            cardSize * 0.5,
-        size: suitSize,
-        paint: cardPlaceholderIconPaint,
-      ),
+          // FoundationPileDiamond
+          RectangleComponent(
+            position: beginCardGap + Vector2(cardSize.x * 6 + cardGap.x * 6, 0),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          SvgComponent(
+            svg: await Svg.load('icons/diamond.svg'),
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(cardSize.x * 6 + cardGap.x * 6, 0) +
+                cardSize * 0.5,
+            size: suitSize,
+            paint: cardPlaceholderIconPaint,
+          ),
 
-      // TableauPile1
-      RectangleComponent(
-        position: beginCardGap + Vector2(0, cardSize.y + cardGap.y),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      TextComponent(
-        text: 'K',
-        anchor: Anchor.center,
-        position:
-            beginCardGap + Vector2(0, cardSize.y + cardGap.y) + cardSize * 0.5,
-        textRenderer: cardPlaceholderTextPaint,
-      ),
+          // TableauPile1
+          RectangleComponent(
+            position: beginCardGap + Vector2(0, cardSize.y + cardGap.y),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          TextComponent(
+            text: 'K',
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(0, cardSize.y + cardGap.y) +
+                cardSize * 0.5,
+            textRenderer: cardPlaceholderTextPaint,
+          ),
 
-      // TableauPile2
-      RectangleComponent(
-        position: beginCardGap +
-            Vector2(cardSize.x + cardGap.x, cardSize.y + cardGap.y),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      TextComponent(
-        text: 'K',
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x + cardGap.x, cardSize.y + cardGap.y) +
-            cardSize * 0.5,
-        textRenderer: cardPlaceholderTextPaint,
-      ),
+          // TableauPile2
+          RectangleComponent(
+            position: beginCardGap +
+                Vector2(cardSize.x + cardGap.x, cardSize.y + cardGap.y),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          TextComponent(
+            text: 'K',
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(cardSize.x + cardGap.x, cardSize.y + cardGap.y) +
+                cardSize * 0.5,
+            textRenderer: cardPlaceholderTextPaint,
+          ),
 
-      // TableauPile3
-      RectangleComponent(
-        position: beginCardGap +
-            Vector2(cardSize.x * 2 + cardGap.x * 2, cardSize.y + cardGap.y),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      TextComponent(
-        text: 'K',
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 2 + cardGap.x * 2, cardSize.y + cardGap.y) +
-            cardSize * 0.5,
-        textRenderer: cardPlaceholderTextPaint,
-      ),
+          // TableauPile3
+          RectangleComponent(
+            position: beginCardGap +
+                Vector2(cardSize.x * 2 + cardGap.x * 2, cardSize.y + cardGap.y),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          TextComponent(
+            text: 'K',
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(
+                  cardSize.x * 2 + cardGap.x * 2,
+                  cardSize.y + cardGap.y,
+                ) +
+                cardSize * 0.5,
+            textRenderer: cardPlaceholderTextPaint,
+          ),
 
-      // TableauPile4
-      RectangleComponent(
-        position: beginCardGap +
-            Vector2(cardSize.x * 3 + cardGap.x * 3, cardSize.y + cardGap.y),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      TextComponent(
-        text: 'K',
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 3 + cardGap.x * 3, cardSize.y + cardGap.y) +
-            cardSize * 0.5,
-        textRenderer: cardPlaceholderTextPaint,
-      ),
+          // TableauPile4
+          RectangleComponent(
+            position: beginCardGap +
+                Vector2(cardSize.x * 3 + cardGap.x * 3, cardSize.y + cardGap.y),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          TextComponent(
+            text: 'K',
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(
+                  cardSize.x * 3 + cardGap.x * 3,
+                  cardSize.y + cardGap.y,
+                ) +
+                cardSize * 0.5,
+            textRenderer: cardPlaceholderTextPaint,
+          ),
 
-      // TableauPile5
-      RectangleComponent(
-        position: beginCardGap +
-            Vector2(cardSize.x * 4 + cardGap.x * 4, cardSize.y + cardGap.y),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      TextComponent(
-        text: 'K',
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 4 + cardGap.x * 4, cardSize.y + cardGap.y) +
-            cardSize * 0.5,
-        textRenderer: cardPlaceholderTextPaint,
-      ),
+          // TableauPile5
+          RectangleComponent(
+            position: beginCardGap +
+                Vector2(cardSize.x * 4 + cardGap.x * 4, cardSize.y + cardGap.y),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          TextComponent(
+            text: 'K',
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(
+                  cardSize.x * 4 + cardGap.x * 4,
+                  cardSize.y + cardGap.y,
+                ) +
+                cardSize * 0.5,
+            textRenderer: cardPlaceholderTextPaint,
+          ),
 
-      // TableauPile6
-      RectangleComponent(
-        position: beginCardGap +
-            Vector2(cardSize.x * 5 + cardGap.x * 5, cardSize.y + cardGap.y),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
-      ),
-      TextComponent(
-        text: 'K',
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 5 + cardGap.x * 5, cardSize.y + cardGap.y) +
-            cardSize * 0.5,
-        textRenderer: cardPlaceholderTextPaint,
-      ),
+          // TableauPile6
+          RectangleComponent(
+            position: beginCardGap +
+                Vector2(cardSize.x * 5 + cardGap.x * 5, cardSize.y + cardGap.y),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          TextComponent(
+            text: 'K',
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(
+                  cardSize.x * 5 + cardGap.x * 5,
+                  cardSize.y + cardGap.y,
+                ) +
+                cardSize * 0.5,
+            textRenderer: cardPlaceholderTextPaint,
+          ),
 
-      // TableauPile7
-      RectangleComponent(
-        position: beginCardGap +
-            Vector2(cardSize.x * 6 + cardGap.x * 6, cardSize.y + cardGap.y),
-        size: cardSize,
-        paint: cardPlaceholderPaint,
+          // TableauPile7
+          RectangleComponent(
+            position: beginCardGap +
+                Vector2(cardSize.x * 6 + cardGap.x * 6, cardSize.y + cardGap.y),
+            size: cardSize,
+            paint: cardPlaceholderPaint,
+          ),
+          TextComponent(
+            text: 'K',
+            anchor: Anchor.center,
+            position: beginCardGap +
+                Vector2(
+                  cardSize.x * 6 + cardGap.x * 6,
+                  cardSize.y + cardGap.y,
+                ) +
+                cardSize * 0.5,
+            textRenderer: cardPlaceholderTextPaint,
+          ),
+        ],
       ),
-      TextComponent(
-        text: 'K',
-        anchor: Anchor.center,
-        position: beginCardGap +
-            Vector2(cardSize.x * 6 + cardGap.x * 6, cardSize.y + cardGap.y) +
-            cardSize * 0.5,
-        textRenderer: cardPlaceholderTextPaint,
-      ),
-    ]);
+    );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    if (_isIniting) {
+      // Fix the world components' position and size according to the current
+      // window size
+      onGameResize(size);
+
+      _isIniting = false;
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:solitaire_dreamy/app.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await windowManager.ensureInitialized();
+
+    windowManager.waitUntilReadyToShow(
+      const WindowOptions(
+        center: true,
+        minimumSize: Size(320, 500),
+        size: Size(461, 720),
+      ),
+      () async {
+        await windowManager.show();
+        await windowManager.focus();
+      },
+    );
+  }
+
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const App());
 }
